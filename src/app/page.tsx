36ed551a2587d39daf6f0b7c9979d54fceee1f73
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-05 18:23:47 +0900

    chore: wrap provider & remove ununsed code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import { MantineProvider } from '@mantine/core';
 import { Metadata } from 'next';
 
 import Mainpage from '@/components/MainPage';
 
 import { SITE_CONFIG } from '@/constants';
-import { getApiResponse } from '@/utils/shared/get-api-response';
-
-import { NpmData, PageParams } from '@/types';
+import Provider from '@/providers';
 
 export const metadata: Metadata = {
   metadataBase: new URL('http://roadmaker.site/'),
@@ -39,38 +38,30 @@ export const metadata: Metadata = {
   },
 };
 
-const loadDataFromApi = async (slug?: string) => {
-  if (slug === 'testError500') {
-    throw new Error('This is mock a ssr 500 test error');
-  }
+// const loadDataFromApi = async () => {
+//   const roadmapPage = 1; // initial load
+//   const [postData] = await Promise.all([
+//     getApiResponse<Postdata>({
+//       apiEndpoint: `${process.env.NEXT_PUBLIC_API}/roadmaps?page=${roadmapPage}&order-type=recent`,
+//       revalidate: 60 * 12, // 30 mins cache
+//     }),
+//   ]);
 
-  // Fetch & cache data from 2 remote APIs test
-  const [reactNpmData, nextJsNpmData] = await Promise.all([
-    getApiResponse<NpmData>({
-      apiEndpoint: 'https://registry.npmjs.org/react/latest',
-      revalidate: 60 * 60 * 24, // 24 hours cache
-    }),
-    getApiResponse<NpmData>({
-      apiEndpoint: 'https://registry.npmjs.org/next/latest',
-      revalidate: 0, // no cache
-    }),
-  ]);
+//   return {
+//     postData,
+//   };
+// };
 
-  return {
-    reactNpmData,
-    nextJsNpmData,
-  };
-};
-
-const AppHome = async ({ searchParams }: PageParams) => {
-  const slug = searchParams?.slug;
-  const { reactNpmData, nextJsNpmData } = await loadDataFromApi(slug);
-
+// const AppHome = async () => {
+const AppHome = () => {
+  // const { postData } = await loadDataFromApi();
   return (
-    <Mainpage
-      reactVersion={reactNpmData?.version}
-      nextJsVersion={nextJsNpmData?.version}
-    />
+    <MantineProvider>
+      <Provider>
+        <Mainpage />
+        {/* <Mainpage postData={postData} /> */}
+      </Provider>
+    </MantineProvider>
   );
 };
 

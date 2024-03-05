eb013ad37a57cd639e1b4b5cd99b504a14e5532c
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-05 18:15:58 +0900

    test: setup basic test

--- src/__tests__/pages/MainPage.test.tsx
+++ src/__tests__/pages/MainPage.test.tsx
@@ -2,7 +2,22 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-import MainPage from '@/components/MainPage';
+import NotFound from '@/app/not-found';
+import { MantineProvider } from '@mantine/core';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 export function mockFetch(data: unknown) {
   return jest.fn().mockImplementation(() =>
@@ -13,12 +28,16 @@ export function mockFetch(data: unknown) {
   );
 }
 
-describe('MainPage', () => {
+describe('Error', () => {
   it('renders the Components', () => {
     window.fetch = mockFetch({});
-    render(<MainPage />);
+    render(
+      <MantineProvider>
+        <NotFound />
+      </MantineProvider>,
+    );
 
-    const text = screen.getByText(/hi!/i);
+    const text = screen.getByText(/home/i);
 
     expect(text).toBeInTheDocument();
   });

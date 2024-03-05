8379d7a9e73ca5c8593c019aeac3fdb13d683fc1
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-05 18:15:21 +0900

    feat: fetch posts in main via useInfiniteQuery

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,12 +1,124 @@
-export default function Mainpage({
-  reactVersion = 'unknown',
-  nextJsVersion = 'unknown',
-}) {
+'use client';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { PropsWithChildren, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+import { getApiResponse } from '@/utils/shared/get-api-response';
+
+import { ArticlesCardsGrid } from './shared/grid/ArticlesCardsGrid';
+
+export interface Member {
+  id: number;
+  email: string;
+  nickname: string;
+  bio: string;
+  avatarUrl: null | string;
+  githubUrl: null | string;
+  blogUrl: null | string;
+  baekjoonId: null | string;
+}
+export interface Post {
+  id: number;
+  title: string;
+  description: string;
+  thumbnailUrl: typeof url;
+  likeCount: number;
+  joinCount: number;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  member: Member;
+}
+export const url = `${process.env.NEXT_PUBLIC_API}/roadmaps?page=2&order-type=recent`;
+export type Postdata = {
+  totalPage: number;
+  next: typeof url;
+  previous: null;
+  result: Array<Post | null>;
+};
+
+export interface PageProps extends PropsWithChildren {
+  postData: Postdata | null;
+}
+const loadDataFromApi = async ({ pageParam }: { pageParam: number }) => {
+  // const loadDataFromApi = async (pageParam: unknown) => {
+  // const roadmapPage = 1; // initial load
+  const [postData] = await Promise.all([
+    getApiResponse<Postdata>({
+      apiEndpoint: `${process.env.NEXT_PUBLIC_API}/roadmaps?page=${pageParam}&order-type=recent`,
+      revalidate: 60 * 12, // 30 mins cache
+    }),
+  ]);
+
+  return {
+    postData,
+  };
+};
+
+export default function Mainpage() {
+  const { ref, inView } = useInView();
+
+  const {
+    data: posts,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ['mainPosts'],
+    queryFn: loadDataFromApi,
+    initialPageParam: 1,
+    getNextPageParam: ({ postData }) => {
+      const { next } = postData! as Postdata;
+      const findPageParamRegex = /page=[0-9]{1,}/g;
+      const searchNext = next && next.match(findPageParamRegex);
+      const pageNum = Number(searchNext && searchNext[0].split('=')[1]);
+      return pageNum;
+    },
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+
+  if (error) {
+    return <>oops something went wrong😑{status}</>;
+  }
+
+  if (isFetchingNextPage) {
+    return <>fetching next page</>;
+  }
+
   return (
-    <main>
-      <section>
-        <div>hi!🌺</div>
-      </section>
-    </main>
+    <>
+      <main>
+        <section>
+          <div>main</div>
+
+          {posts?.pages.map(({ postData }, index) =>
+            postData.next ? (
+              <ArticlesCardsGrid
+                key={index}
+                postData={postData?.result}
+                innerRef={ref}
+              />
+            ) : (
+              <ArticlesCardsGrid key={index} postData={postData?.result} />
+            ),
+          )}
+
+          {/* {posts?.pages.map(({ postData }, index) => (
+            <ArticlesCardsGrid
+              key={index}
+              postData={postData?.result}
+              innerRef={ref}
+            />
+          ))} */}
+        </section>
+      </main>
+    </>
   );
 }
--- src/components/shared/grid/ArticlesCardsGrid.tsx
+++ src/components/shared/grid/ArticlesCardsGrid.tsx
@@ -7,57 +7,93 @@ import {
   SimpleGrid,
   Text,
 } from '@mantine/core';
+import { PropsWithChildren } from 'react';
 
 import classes from './ArticlesCardsGrid.module.css';
 
-const mockdata = [
-  {
-    title: 'Top 10 places to visit in Norway this summer',
-    image:
-      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 18, 2022',
-  },
-  {
-    title: 'Best forests to visit in North America',
-    image:
-      'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 27, 2022',
-  },
-  {
-    title: 'Hawaii beaches review: better than you think',
-    image:
-      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 9, 2022',
-  },
-  {
-    title: 'Mountains at night: 12 best locations to enjoy the view',
-    image:
-      'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 12, 2022',
-  },
-];
+import { Post } from '@/components/MainPage';
 
-export function ArticlesCardsGrid() {
-  const cards = mockdata.map((article) => (
-    <Card
-      key={article.title}
-      p='md'
-      radius='md'
-      component='a'
-      href='#'
-      className={classes.card}
-    >
-      <AspectRatio ratio={1920 / 1080}>
-        <Image src={article.image} alt={`${article.image} 이미지`} />
-      </AspectRatio>
-      <Text c='dimmed' size='xs' tt='uppercase' fw={700} mt='md'>
-        {article.date}
-      </Text>
-      <Text className={classes.title} mt={5}>
-        {article.title}
-      </Text>
-    </Card>
-  ));
+export interface PostProps extends PropsWithChildren {
+  postData: Post[];
+  innerRef?: (node?: Element | null) => void;
+}
+
+export function ArticlesCardsGrid({ postData, innerRef }: PostProps) {
+  const data = postData as Post[];
+  if (!data) {
+    return <div>No items</div>;
+  }
+
+  const cards = data.map(
+    (article, i) =>
+      i === data.length - 1 ? (
+        <Card
+          key={article?.id}
+          p='md'
+          ref={innerRef}
+          radius='md'
+          component='a'
+          href={`/roadmap/post/${article?.id}`}
+          className={classes.card}
+        >
+          <AspectRatio ratio={1920 / 1080}>
+            <Image
+              src={article?.thumbnailUrl}
+              alt={`${article?.thumbnailUrl} 이미지`}
+            />
+          </AspectRatio>
+          <Text c='dimmed' size='xs' tt='uppercase' fw={700} mt='md'>
+            {`${article?.createdAt}`}
+          </Text>
+          <Text className={classes.title} mt={5}>
+            {article?.title}
+          </Text>
+        </Card>
+      ) : (
+        <Card
+          key={article?.id}
+          p='md'
+          radius='md'
+          component='a'
+          href={`/roadmap/post/${article?.id}`}
+          className={classes.card}
+        >
+          <AspectRatio ratio={1920 / 1080}>
+            <Image
+              src={article?.thumbnailUrl}
+              alt={`${article?.thumbnailUrl} 이미지`}
+            />
+          </AspectRatio>
+          <Text c='dimmed' size='xs' tt='uppercase' fw={700} mt='md'>
+            {`${article?.createdAt}`}
+          </Text>
+          <Text className={classes.title} mt={5}>
+            {article?.title}
+          </Text>
+        </Card>
+      ),
+    // <Card
+    //   key={article?.id}
+    //   p='md'
+    //   radius='md'
+    //   component='a'
+    //   href={`/roadmap/post/${article?.id}`}
+    //   className={classes.card}
+    // >
+    //   <AspectRatio ratio={1920 / 1080}>
+    //     <Image
+    //       src={article?.thumbnailUrl}
+    //       alt={`${article?.thumbnailUrl} 이미지`}
+    //     />
+    //   </AspectRatio>
+    //   <Text c='dimmed' size='xs' tt='uppercase' fw={700} mt='md'>
+    //     {`${article?.createdAt}`}
+    //   </Text>
+    //   <Text className={classes.title} mt={5}>
+    //     {article?.title}
+    //   </Text>
+    // </Card>
+  );
 
   return (
     <Container py='xl'>

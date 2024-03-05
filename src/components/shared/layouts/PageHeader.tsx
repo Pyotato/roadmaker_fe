61c7ccdfdf3ceca70646815b1d455ce0e85c4239
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-05 18:13:44 +0900

    chore: remove unused code

--- src/components/shared/layouts/PageHeader.tsx
+++ src/components/shared/layouts/PageHeader.tsx
@@ -1,20 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
 const PageHeader = () => {
-  // const loadDataFromApi = async () => {
-  //   const roadmapPage = 1; // initial load
-  //   const [postData] = await Promise.all([
-  //     getApiResponse<Postdata>({
-  //       apiEndpoint: `${process.env.NEXT_PUBLIC_API}/roadmaps?page=${roadmapPage}&order-type=recent`,
-  //       revalidate: 60 * 12, // 30 mins cache
-  //     }),
-  //   ]);
-
-  //   return {
-  //     postData,
-  //   };
-  // };
-
   const router = useRouter();
   return (
     <section>

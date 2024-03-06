c33146bb0ac635d3be5b683036586d6fa85afd98
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-06 19:27:24 +0900

    chore: remove comments

--- src/app/roadmap/post/components/roadmapInfo/reactFlow/ReactFlow.tsx
+++ src/app/roadmap/post/components/roadmapInfo/reactFlow/ReactFlow.tsx
@@ -29,13 +29,6 @@ const ReactFlowRoadmap = ({ reactFlowInfo }: ReactFlowProps) => {
 
   const [nodeState, setNodes, onNodesChange] = useNodesState<Node[]>([]);
   const [edgeState, setEdges, onEdgesChange] = useEdgesState<Edge[]>([]);
-
-  /**
-   * reactflow의 ReactFlow는 반드시 수치로 나타낸 height요소가 필요하지만,
-   * div 영역 안을 스크롤 하는 방식이 아닌 fit-content과 같은 효과를 내기 위해
-   * node의 첫요소와 마지막 요소와 간의 간격을 구해 높이를 동적으로 구할 수 있도록 했습니다.
-   *
-   */
   const getMinMax = <T extends ReactFlowInfo['nodes']>(obj: T, arg: string) =>
     obj.reduce(
       (acc, curr) => {
@@ -50,10 +43,6 @@ const ReactFlowRoadmap = ({ reactFlowInfo }: ReactFlowProps) => {
   const getHeight = getMinMax(nodes, 'height');
   const getWidth = getMinMax(nodes, 'width');
 
-  /**
-   * 그래프 노드의 최대 최소 넓이와 높이를 구한 값과, offset (요소 테두리에 여백을 주기 위함)을 통해
-   * ReactFlow의 width와 height 속성을 줄 수 있습니다.
-   */
   const getRangePx = (obj: { max: number; min: number }, offset: number) =>
     `${obj.max + obj.min + offset}px`;
 

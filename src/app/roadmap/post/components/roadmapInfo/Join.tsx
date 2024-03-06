3c62f142d656ba855edae1883c9834ce03468ba5
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-06 18:20:13 +0900

    feat: add joined/join roadmap feature

---
+++ src/app/roadmap/post/components/roadmapInfo/Join.tsx
@@ -0,0 +1,24 @@
+import { PropsWithChildren } from 'react';
+
+import { toTSXString } from '@/utils/shared';
+
+import { AboutInfo } from '.';
+
+interface JoinProps extends PropsWithChildren {
+  joinInfo: {
+    isJoined: AboutInfo['isJoined'];
+    joinCount: AboutInfo['joinCount'];
+  };
+}
+
+const Join = ({ joinInfo }: JoinProps) => {
+  const { isJoined, joinCount } = joinInfo;
+
+  return (
+    <>
+      <button type='button'>{isJoined ? '참여중' : '참여하기'}</button>
+      <h3>참여인원: {toTSXString(joinCount)}명</h3>
+    </>
+  );
+};
+export default Join;

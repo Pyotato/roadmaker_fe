197675e65f844c74d858f3b330ced039dc04062b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-06 18:59:35 +0900

    docs: add explanation for code

--- src/utils/shared/index.ts
+++ src/utils/shared/index.ts
@@ -1,3 +1,7 @@
+/**
+ * 타입스크립트 Omit와 같이 동작하지만
+ * Object의 키를 string의 args로 받아 온 값을 제외하고 새로운 Object를 반환하는 함수
+ */
 export const omit = <T extends Record<string, unknown>>(
   obj: T,
   ...args: Array<keyof T>
@@ -15,6 +19,10 @@ export const omit = <T extends Record<string, unknown>>(
   );
 };
 
+/**
+ * 타입스크립트 Pick과 같이 동작하지만
+ * Object의 키를 string의 args로 받아 온 값만을 취하고 새로운 Object를 반환하는 함수
+ */
 export const pick = <T extends Record<string, unknown>>(
   obj: T,
   ...args: Array<keyof T>

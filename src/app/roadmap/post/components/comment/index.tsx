28172e7fa6e4e9e14cbce50753a1fa1d4e31852b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-07 19:51:06 +0900

    feat: #15 style comment section

--- src/app/roadmap/post/components/comment/index.tsx
+++ src/app/roadmap/post/components/comment/index.tsx
@@ -1,10 +1,18 @@
-import Comment from '@/app/roadmap/post/components/comment/Comment';
+import { Container, Title } from '@mantine/core';
+
 import CommentBox from '@/app/roadmap/post/components/comment/CommentBox';
+import Comment from '@/app/roadmap/post/components/comment/Comments';
 
 export default function CommentSection() {
   return (
     <>
+      <Container py='l'>
+        <Title order={1}>댓글 작성</Title>
+      </Container>
       <CommentBox />
+      <Container py='l'>
+        <Title order={1}>댓글</Title>
+      </Container>
       <Comment />
     </>
   );

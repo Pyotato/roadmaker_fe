27e000d7b45548a86a97139c21c8663e44ff4682
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-25 18:10:09 +0900

    chore: creaet util function for localstorage

---
+++ src/utils/shared/localStorage.ts
@@ -0,0 +1,25 @@
+import { consoleLog } from './console-log';
+
+export const setItem = (key: string, item: unknown) => {
+  try {
+    window.localStorage.setItem(key, JSON.stringify(item));
+  } catch (err) {
+    consoleLog('local storage setItem error:', err);
+  }
+};
+
+export const getItem = (key: string) => {
+  try {
+    return window.localStorage.getItem(key);
+  } catch (err) {
+    consoleLog('local storage getItem error:', err);
+  }
+};
+
+export const removeItem = (key: string) => {
+  try {
+    return window.localStorage.removeItem(key);
+  } catch (err) {
+    consoleLog('local storage removeItem error:', err);
+  }
+};

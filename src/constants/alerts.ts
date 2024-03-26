a73f92ba5acf9cd8e091ca1190b7b15294f2a8c4
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-03-26 20:45:31 +0900

    chore: add alert message for auth

--- src/constants/alerts.ts
+++ src/constants/alerts.ts
@@ -24,6 +24,12 @@ export const missing = {
       '내용은 필수입니다. 내용을 입력해주세요. \n 빈칸과 공백 이외의 내용을 입력해주세요.',
     color: 'orange',
   },
+  auth: {
+    id: 'no-auth-alert',
+    title: '⚠️ 로그인이 필요합니다. ⚠️',
+    message: '로그인 후 이용 가능합니다.',
+    color: 'orange',
+  },
 };
 
 export type Missing = typeof missing;
@@ -41,7 +47,23 @@ export const sucess = {
     title: '로드맵 생성 성공',
     color: 'teal',
   },
+  signup: {
+    id: `signup-sucess`,
+    title: '회원 가입 성공',
+    color: 'teal',
+  },
 };
 
 export type Success = typeof sucess;
 export type SuccessKeys = keyof Success;
+
+export const fail = {
+  409: {
+    id: 'roadmap-signup-error',
+    title: '⚠️ 중복 ⚠️',
+    color: 'red',
+  },
+};
+
+export type Fail = typeof fail;
+export type FailKeys = keyof Fail;

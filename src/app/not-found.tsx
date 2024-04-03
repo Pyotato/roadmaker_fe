4f9564d2329dd855d34db55c7daadf30950c349b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-04-04 00:15:44 +0900

    chore: style 404 and error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,18 @@
 import { Metadata } from 'next';
-import { RiAlarmWarningFill } from 'react-icons/ri';
+
+import NotFound from '@/components/NotFound';
 
 export const metadata: Metadata = {
   title: 'Not Found',
 };
 
 export default function NotFoundPage() {
+  const title = 'Something is not right...';
+  const message =
+    'Page you are trying to open does not exist. You may have mistyped the address, or the page has been moved to another URL. If you think this is an error contact support.';
   return (
     <main>
-      <div>
-        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <RiAlarmWarningFill
-            size={60}
-            className='drop-shadow-glow animate-flicker text-red-500'
-          />
-          <h1>Page Not Found</h1>
-          <h5>change this in app/not-found.tsx</h5>
-          <a href='/'>Back to home</a>
-        </div>
-      </div>
+      <NotFound title={title} message={message} />
     </main>
   );
 }

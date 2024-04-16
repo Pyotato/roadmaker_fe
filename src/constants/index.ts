24bc61908b25ec4db6146514b5db4f81a0ba8278
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-04-16 12:36:25 +0900

    refactor: replace string to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export * from '@/constants/alerts';
 export * from '@/constants/config';
 export * from '@/constants/edges';
 export * from '@/constants/env';
+export * from '@/constants/localStorageKey';
 export * from '@/constants/nodes';
 export * from '@/constants/regex';
 export * from '@/constants/routes';
